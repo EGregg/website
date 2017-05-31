07d98fc2616e07c03b80d99bc418c6b8a780fd8b
EGregg <creosino@gmail.com>
2017-05-31 00:06:18 -0400

    background color changes based on time

--- js/playGround.js
+++ js/playGround.js
@@ -1,19 +1,20 @@
 var time = new Date();
-if (time.getHours() < 8){
+if (time.getHours() <= 18 ){
    console.log(time.getHours());
-   document.body.style.backgroundColor = "white";
+    document.body.style.backgroundColor = "#F5F5F5";
    console.log("it's going to be a long day");
 }
 
-else if (time.getHours() <= 8 && time.getHours <= 16){
+else if (time.getHours() >= 19 && time.getHours <= 21){
     console.log(time.getHours());
-    document.body.style.backgroundColor = "gray";
+    document.body.style.backgroundColor = "9E9E9E";
 }
 
 else{
    
    console.log(time.getHours());
-   document.body.style.backgroundColor = body.style.backgroundColor;
+   console.log("when all other colors fail");
+   document.body.style.backgroundColor = "#2E2E2E";
 }
 
 
